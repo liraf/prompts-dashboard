fefcec988d30b660cf9d69c651c29470a2e0db7d
Felipe Lira <felipelira1908@hotmail.com>
2023-06-15 21:48:12 -0300

    feat: add tokens info to prompt

--- components/Prompt.vue
+++ components/Prompt.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="w-full bg-white px-4 py-4 rounded-lg">
-    <h3 class="text-blue-950 mb-2">Prompt</h3>
+  <div class="w-full bg-white px-4 py-4 rounded-lg text-blue-950">
+    <h3 class="mb-2">Prompt</h3>
     <CustomTextarea />
+    <div class="mt-2">Tokens: <b>{{ tokens }}</b></div>
   </div>
 </template>
 
 <script setup>
+const tokens = ref(22)
 </script>
