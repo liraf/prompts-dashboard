19b4a8fbdc0bb041c152eb19cde2a7a8214fa5a5
Felipe Lira <felipelira1908@hotmail.com>
2023-06-18 15:26:39 -0300

    feat: replace variable with pill

--- components/ParsedPrompt.vue
+++ components/ParsedPrompt.vue
@@ -1,7 +1,9 @@
 <template>
-  <div @click="() => $emit('click')" class="min-h-[64px] w-full bg-gray-100 rounded-md text-blue-950 px-4 py-2 whitespace-pre-wrap">
-    {{ prompt }}
-  </div>
+  <div
+    @click="() => $emit('click')"
+    class="min-h-[64px] w-full bg-gray-100 rounded-md text-blue-950 px-4 py-2 whitespace-pre-wrap"
+    v-html="parsedPrompt"
+  />
 </template>
 
 <script setup lang="ts">
@@ -12,4 +14,18 @@ interface ParsedPromptProps {
 }
 
 const { prompt } = defineProps<ParsedPromptProps>()
+
+const parsedPrompt = computed(() => {
+  let newParsedPrompt = prompt
+
+  // TODO: Use the 'Pill' component
+  const pillClassName = 'rounded-full px-3 py-1 bg-blue-950 text-white inline'
+
+  const variables = newParsedPrompt.match(/\{{[a-zA-Z0-9_ ]*}}/g)
+  variables?.forEach((variable) => {
+    newParsedPrompt = newParsedPrompt.split(variable).join(`<span class="${pillClassName}">${variable.slice(2,-2).trim()}</span>`)
+  })
+
+  return newParsedPrompt
+})
 </script>
