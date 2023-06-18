2639d9b5ce89eae352b944e07d9c925b03d76f4a
Felipe Lira <felipelira1908@hotmail.com>
2023-06-18 14:54:41 -0300

    feat: make prompt follow textarea height

--- components/Input.vue
+++ components/Input.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- TODO: make it automatically resizable with a custom composable -->
   <input
     class="w-full bg-gray-100 rounded-md text-blue-950 px-4 py-2 focus:outline-none focus:ring focus:ring-blue-500"
   />
--- components/ParsedPrompt.vue
+++ components/ParsedPrompt.vue
@@ -1,5 +1,5 @@
 <template>
-  <div @click="() => $emit('click')" class="min-h-8 w-full bg-gray-100 rounded-md text-blue-950 px-4 py-2">
+  <div @click="() => $emit('click')" class="min-h-[64px] w-full bg-gray-100 rounded-md text-blue-950 px-4 py-2 whitespace-pre-wrap">
     {{ prompt }}
   </div>
 </template>
