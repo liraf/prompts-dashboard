101b9098f95b8efbaa29d8414da11832dd1e5df6
Felipe Lira <felipelira1908@hotmail.com>
2023-06-22 00:59:22 -0300

    feat: add new font family

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,24 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          crossorigin: "",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+        },
+      ],
+    }
   }
 })
